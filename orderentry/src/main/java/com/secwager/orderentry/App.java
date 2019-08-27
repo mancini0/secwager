149e699d5dd8f394c66eec7246bd95ee0f36060b
mike.mancini <mancini0@gmail.com>
2019-08-27 10:01:07 -0500

    add more methods to cashier service, add main method to orderentry service

--- orderentry/src/main/java/com/secwager/orderentry/App.java
+++ orderentry/src/main/java/com/secwager/orderentry/App.java
@@ -1,11 +1,14 @@
 package com.secwager.orderentry;
 
+import com.secwager.common.GrpcServer;
+import com.secwager.orderentry.di.DaggerOrderEntryComponent;
+
 public class App {
 
 
   public static void main(String[] args) throws Exception {
-//    GrpcServer server = new GrpcServer(0,
-//        DaggerCapitalizationComponent.create().capitalizationService());
-//    server.start();
+    GrpcServer server = new GrpcServer(0,
+        DaggerOrderEntryComponent.create().buildOrderEntryService());
+    server.start();
   }
 }
